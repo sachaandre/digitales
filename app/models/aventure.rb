2089e5eee5a0dc68a3bc711a5012ea9963aaa875
Sacha <andresachaa@gmail.com>
2017-01-23 23:27:21 +0100

    Changing home page, changing portrait page

--- app/models/aventure.rb
+++ app/models/aventure.rb
@@ -2,6 +2,6 @@ class Aventure < ApplicationRecord
   has_and_belongs_to_many :indefinitions
   has_and_belongs_to_many :variables
 
-  has_attached_file :aventure_portrait, styles: { medium: "400x500#", thumb: "200x250#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :aventure_portrait, styles: { large: "480x600#", medium: "400x500#", thumb: "200x250#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :aventure_portrait, content_type: /\Aimage\/.*\z/
 end
