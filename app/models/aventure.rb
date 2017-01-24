36ca03515db29857b20904fea83c3b6d1e8c6b11
Sacha <andresachaa@gmail.com>
2017-01-24 09:55:46 +0100

    Correcting Home Page, Styling Portrait

--- app/models/aventure.rb
+++ app/models/aventure.rb
@@ -4,4 +4,5 @@ class Aventure < ApplicationRecord
 
   has_attached_file :aventure_portrait, styles: { large: "480x600#", medium: "400x500#", thumb: "200x250#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :aventure_portrait, content_type: /\Aimage\/.*\z/
+
 end
