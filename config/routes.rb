fbaddd13707c41e3b5d694e27bd6016b70cd721a
Sacha Andre <andresachaa@gmail.com>
2017-04-03 14:53:48 +0200

    Changing signin signout routes; Putting devise links on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+
   resources :tags
   resources :indefinitions
   resources :variables
   resources :aventures
+
+	root 'static_pages#home_page'
 
   resources :variables do
     resources :aventures, only: [:index]
@@ -13,7 +15,13 @@ Rails.application.routes.draw do
     resources :aventures, only: [:index]
   end
 
-  root 'static_pages#home_page'
+
+	devise_for :users
+	as :user do
+  	get 'signin', to: 'devise/sessions#new', as: :signin
+  	get 'signup', to: 'devise/registrations#new', as: :signup
+  	delete 'signout', to: 'devise/sessions#destroy', as: :signout
+	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
